2f36837ec87424c9d2d58923438fce7fd9af8626
Vincent <vincecreton@hotmail.fr>
2020-04-24 13:33:12 +0200

    50% de la page reflexe.html (icons et layout OK)

--- JS/navbar_anim.js
+++ JS/navbar_anim.js
@@ -1,12 +1,17 @@
 let navbar = document.querySelector('#mobile-navbar')
 let mobile_wrapper = document.querySelector("#wrapper-mobile")
 let front_grid = document.querySelector(".front-grid")
+let reflexe_grid = document.querySelector(".reflexe-grid")
 let navbar_open = false
 
 function navbarappear() {
     navbar.classList.add('visible')
     mobile_wrapper.classList.add('blockY')
-    front_grid.classList.add('blur')
+    if(front_grid){
+        front_grid.classList.add('blur')
+    } else if(reflexe_grid) {
+        reflexe_grid.classList.add('blur')
+    }
     navbar_open = true
 }
 
@@ -14,7 +19,11 @@ function navbardisappear() {
     if(navbar_open){
         navbar.classList.remove('visible')
         mobile_wrapper.classList.remove('blockY')
-        front_grid.classList.remove('blur')
+        if(front_grid) {
+            front_grid.classList.remove('blur')
+        } else if(reflexe_grid) {
+            reflexe_grid.classList.remove('blur')
+        }
         navbar_open = false
     }
 }
