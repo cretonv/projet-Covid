3b1b23dc66e922c3460332466fa414bea9f967f8
Vincent <vincecreton@hotmail.fr>
2020-04-26 11:12:07 +0200

    Création et export de la classe FakeNews

---
+++ JS/class/FakeNews.js
@@ -0,0 +1,26 @@
+class FakeNews {
+
+    news_Content
+    source_name
+    real_source_name
+
+    constructor(news_Content, source_name, real_source_name) {
+        this.news_Content = news_Content
+        this.source_name = source_name
+        this._real_source_name = real_source_name
+    }
+
+    get news_Content() {
+        return this.news_Content
+    }
+
+    get source_name() {
+        return this.source_name
+    }
+
+    get real_source_name() {
+        return this._real_source_name;
+    }
+}
+
+export {FakeNews}
---
+++ JS/fake_news_data.js
@@ -0,0 +1,1 @@
+import {FakeNews} from './class/FakeNews'
