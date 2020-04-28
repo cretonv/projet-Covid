84df6329eb8fa993646b8dc52d5797c1758b7505
Vincent <vincecreton@hotmail.fr>
2020-04-28 17:03:32 +0200

    Texte d'into rédigés

--- JS/reflexesInfos_anim.js
+++ JS/reflexesInfos_anim.js
@@ -1,10 +1,21 @@
 let reflexe_info = document.querySelectorAll('.reflexe-info')
 let reflexe1 = document.querySelector('#reflexe1')
+let infosOpen = false
 
 function infos_appear(id) {
+    infos_disappearAll()
     reflexe_info[id].classList.add('open')
+    infosOpen = true
 }
 
 function infos_disappear(id) {
     reflexe_info[id].classList.remove('open')
+    infosOpen = false
+}
+
+function infos_disappearAll() {
+    if(infosOpen){
+        reflexe_info.forEach(element => element.classList.remove('open'))
+        infosOpen = false
+    }
 }
